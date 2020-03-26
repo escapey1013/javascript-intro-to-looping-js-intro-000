5c00ff0eaf6c792c0168bcce08f0f46bbe84b9cc
Simon Kilroy <simonkilroy1@gmail.com>
2020-03-26 16:49:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,18 +1,35 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     if (i === 1) {
       array.push("I am 1 strange loop.")
     } else {
       array.push (`I am ${i} strange loops.`)
     }
   }
+  return array
 }
 
 
+function whileLoop(n) {
+  while (n > 0) {
+      console.log(--n);
+  }
+  return "done";
+}
 
 
+var i = 0;
 
+function incrementVariable() {
+  i = i + 1;
+}
 
+function doWhileLoop(array) {
+  do {
+    array.shift();
+  } while (array.length > 0 && incrementVariable());
+  return array
+}
 
 
 
@@ -46,15 +63,3 @@ function forLoop(array) {
 //   return 'done'
 // }
 //
-// var i = 0;
-//
-// function incrementVariable() {
-//   i = i + 1;
-// }
-//
-// function doWhileLoop(array) {
-//   do {
-//     array.shift();
-//   } while (array.length > 0 && incrementVariable());
-//   return array
-// }
